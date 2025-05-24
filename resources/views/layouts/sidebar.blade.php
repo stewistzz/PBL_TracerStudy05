e560a6b17a88cd0608c1a30227b089d36af112c8
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2025-05-24 12:20:56 +0700

    improve dashboard, except arrow untuk indikator masih error

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,12 +7,25 @@
                 <span class="menu-title">Dashboard</span>
             </a>
         </li>
+        {{-- sidebar Sebaran untuk admin --}}
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
                 <i class="mdi mdi-arrow-expand-all menu-icon"></i>
                 <span class="menu-title">Sebaran</span>
+                <i class="menu-arrow"></i>
             </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"><a class="nav-link" href="pages/ui-features/buttons.html">Jenis Profesi</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="pages/ui-features/dropdowns.html">Profesi</a></li>
+                    <li class="nav-item"><a class="nav-link" href="pages/ui-features/typography.html">Kesesuaian</a>
+                    </li>
+                </ul>
+            </div>
         </li>
+        {{-- end sebaran --}}
+
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false"
                 aria-controls="form-elements">
@@ -20,28 +33,64 @@
                 <span class="menu-title">Masa Tunggu</span>
             </a>
         </li>
+
+        {{-- sidebar Penilaian untuk admin --}}
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
                 <i class="mdi mdi-star-circle menu-icon"></i>
-                <span class="menu-title">Penilaian Pengguna</span>
+                <span class="menu-title">Pengguna</span>
+                <i class="menu-arrow"></i>
             </a>
+            <div class="collapse" id="charts">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"><a class="nav-link" href="pages/ui-features/buttons.html">Tabel Kepuasan</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="pages/ui-features/dropdowns.html">Grafik Kepuasan</a>
+                    </li>
+                </ul>
+            </div>
         </li>
+        {{-- end penilaian --}}
+
+        {{-- alumni sidebar admin --}}
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
                 <i class="mdi mdi-account-group menu-icon"></i>
                 <span class="menu-title">Alumni</span>
+                <i class="menu-arrow"></i>
             </a>
+            <div class="collapse" id="tables">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"><a class="nav-link" href="#">Data Alumni</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#">Data Tracer</a>
+                    </li>
+                </ul>
+            </div>
         </li>
+        {{-- end sidebar alumni --}}
+
+
+        {{-- sidebar Kuisioner admin --}}
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
                 <i class="mdi mdi-comment-question-outline menu-icon"></i>
                 <span class="menu-title">Kuisioner</span>
+                <i class="menu-arrow"></i>
             </a>
+            <div class="collapse" id="icons">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="#">Alumni</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Pengguna</a></li>
+                </ul>
+            </div>
         </li>
+        {{-- end kuisioner admin --}}
+
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                <i class="mdi mdi-file-chart menu-icon"></i>
-                <span class="menu-title">Laporan</span>
+                <i class="mdi mdi-account-key menu-icon"></i>
+                <span class="menu-title">Admin</span>
             </a>
         </li>
 
