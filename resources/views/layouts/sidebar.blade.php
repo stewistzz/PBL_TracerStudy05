5c58f208ce35dd9b5257a3de414a6ff3f3ecbb20
Nova Diana <nopadr12@gmail.com>
2025-06-08 18:09:01 +0700

    make sidebar sticky on scroll

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
                 <span class="menu-title">Dashboard</span>
             </a>
         </li>
-
+        
         {{-- sidebar Sebaran untuk admin --}}
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
@@ -17,9 +17,13 @@
             </a>
             <div class="collapse" id="ui-basic">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"><a class="nav-link" href="{{ route('kategori_profesi.index') }}">Jenis Profesi</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('kategori_profesi.index') }}">Jenis
+                            Profesi</a>
+                    </li>
                     <li class="nav-item"><a class="nav-link" href="{{ url('/profesi') }}">Profesi</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('admin.kesesuaian') }}">Kesesuaian</a></li>
+
+                    <li class="nav-item"><a class="nav-link" href="{{ route('admin.kesesuaian') }}">Kesesuaian</a>
+                    </li>
                 </ul>
             </div>
         </li>
@@ -35,7 +39,8 @@
 
         {{-- sidebar masa tunggu --}}
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+            <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false"
+                aria-controls="form-elements">
                 <i class="mdi mdi-timer-sand menu-icon"></i>
                 <span class="menu-title">Masa Tunggu</span>
             </a>
@@ -50,33 +55,37 @@
             </a>
             <div class="collapse" id="charts">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"><a class="nav-link" href="{{ route('data_pengguna.index') }}">Data Pengguna</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('kepuasan.index') }}">Tabel Kepuasan</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('kepuasan.grafik') }}">Grafik Kepuasan</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('jawaban_pengguna.index') }}">Jawaban Pengguna</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('data_pengguna.index') }}">Data Pengguna</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('kepuasan.index') }}">Tabel Kepuasan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('kepuasan.grafik') }}">Grafik Kepuasan</a>
+                    </li>
                 </ul>
             </div>
         </li>
         {{-- end penilaian --}}
 
-      {{-- alumni sidebar admin --}}
-<li class="nav-item">
-    <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-        <i class="mdi mdi-account-group menu-icon"></i>
-        <span class="menu-title">Alumni</span>
-        <i class="mdi mdi-menu-down ml-auto"></i>
-    </a>
-    <div class="collapse" id="tables">
-        <ul class="nav flex-column sub-menu">
-            <li class="nav-item"><a class="nav-link" href="{{ route('alumni.index') }}">Data Alumni</a></li>
-            <li class="nav-item"><a class="nav-link" href="{{ route('alumni_tracer.index') }}">Data Tracer</a></li>
-            <li class="nav-item"><a class="nav-link" href="{{ route('jawaban_alumni.index') }}">Jawaban Alumni</a></li>
-        </ul>
-    </div>
-</li>
+        {{-- alumni sidebar admin --}}
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
+                <i class="mdi mdi-account-group menu-icon"></i>
+                <span class="menu-title">Alumni</span>
+                <i class="mdi mdi-menu-down ml-auto"></i>
+            </a>
+            <div class="collapse" id="tables">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"><a class="nav-link" href="{{ route('alumni.index') }}">Data Alumni</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('alumni_tracer.index') }}">Data Tracer</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
         {{-- end sidebar alumni --}}
 
-        {{-- Data User --}}
+        {{-- Data Ueer --}}
         <li class="nav-item">
             <a class="nav-link" href="{{ route('user.index') }}">
                 <i class="mdi mdi-account-group menu-icon"></i>
@@ -93,8 +102,10 @@
             </a>
             <div class="collapse" id="icons">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"><a class="nav-link" href="{{ route('kategori_pertanyaan.index') }}">Kategori</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('pertanyaan.index') }}">Pertanyaan</a></li>
+                    <li class="nav-item"> <a class="nav-link"
+                            href="{{ route('kategori_pertanyaan.index') }}">Kategori</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ route('pertanyaan.index') }}">Pertanyaan</a>
+                    </li>
                 </ul>
             </div>
         </li>
@@ -108,7 +119,8 @@
         </li>
 
         <li class="nav-item">
-            <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <a href="#" class="nav-link"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <i class="mdi mdi-logout menu-icon"></i>
                 <span class="menu-title">Logout</span>
             </a>
@@ -117,6 +129,20 @@
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
+
+
     </ul>
 </nav>
-<!-- partial -->
+
+<style>
+    /* Buat sidebar tetap di kiri dan menempel saat scroll */
+    .sidebar {
+        position: fixed;
+    }
+
+    /* Tambahkan padding/margin pada konten utama agar tidak tertutup sidebar */
+    .main-panel {
+        margin-left: 250px;
+        /* sesuaikan dengan lebar sidebar */
+    }
+</style>
