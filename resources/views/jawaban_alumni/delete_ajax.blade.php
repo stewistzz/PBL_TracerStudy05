56bc10d55bded6337dc0066477bad556d440197e
afgan galih <afgangalihf@gmail.com>
2025-06-12 10:29:39 +0700

    fix: jawaban_alumni/delete_ajax.blade

--- resources/views/jawaban_alumni/delete_ajax.blade.php
+++ resources/views/jawaban_alumni/delete_ajax.blade.php
@@ -2,9 +2,7 @@
     <div class="modal-content">
         <div class="modal-header bg-danger text-white">
             <h5 class="modal-title">Konfirmasi Hapus</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
-            </button>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
             <p>Apakah Anda yakin ingin menghapus jawaban ini?</p>
@@ -12,7 +10,7 @@
             <p><strong>Jawaban:</strong> {{ $jawaban->jawaban }}</p>
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
             <form action="{{ route('jawaban_alumni.delete_ajax', $jawaban->jawaban_id) }}" method="POST" style="display:inline;">
                 @csrf
                 @method('DELETE')
